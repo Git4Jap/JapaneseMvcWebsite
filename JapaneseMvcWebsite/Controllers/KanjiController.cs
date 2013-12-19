a81daef993f90ec2b0dfd6c963aa5e6e0b03cbd2
Git4Jap <neogeoman@hotmail.com>
2013-12-19 23:59:42 +0000

    Initial changes

---
+++ JapaneseMvcWebsite/Controllers/KanjiController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JapaneseMvcWebsite.JapaneseService;
+
+namespace JapaneseMvcWebsite.Controllers
+{
+    public class KanjiController : Controller
+    {
+        // GET: /Kanji/
+        public ActionResult AddKanji()
+        {
+            return View();
+        }
+
+        // GET: /Kanji/
+        [HttpPost]
+        public ActionResult AddKanji(Kanji newKanji)
+        {
+            if (ModelState.IsValid)
+            {
+                var serviceClient = new JapaneseServiceClient();
+                serviceClient.InsertKanji(newKanji);
+            }
+
+            return View(newKanji);
+        }
+    }
+}
